00d3e3384ffc64eee17808d6b8c769308084ff54
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-03-30 22:44:29 +0530

    theme: add darker, cardColor, and mainColor constants

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 const primary = Color(0xFF333333);
 const secondary = Color(0xFFd95b44);
+
+const darker = Color(0xFF3E4249);
+const cardColor = Colors.white;
+const mainColor = Color(0xFF000000);
 const appBgColor = Color(0xFFFAFAFA);
