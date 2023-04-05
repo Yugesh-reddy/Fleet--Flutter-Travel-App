f200074783714bc177f974bbf8c0a0bb90fb6d0c
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-04-05 16:16:32 +0530

    feat: create base Flutter entry point in lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Travel App',
+      home: Scaffold(
+        body: Center(child: Text('Fleet Travel App')),
+      ),
+    );
+  }
+}
