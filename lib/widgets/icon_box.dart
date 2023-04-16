e4a2c095a4905b0b727dd355cf469c8cbab9ae0d
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-04-16 13:11:24 +0530

    widgets: add bgColor and padding parameters to IconBox

--- lib/widgets/icon_box.dart
+++ lib/widgets/icon_box.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 class IconBox extends StatelessWidget {
-  const IconBox({Key? key, required this.child}) : super(key: key);
+  const IconBox({
+    Key? key,
+    required this.child,
+    this.bgColor,
+    this.padding = 5,
+  }) : super(key: key);
+
   final Widget child;
+  final Color? bgColor;
+  final double padding;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(padding),
+      color: bgColor,
       child: child,
     );
   }
