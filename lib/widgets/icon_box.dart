d6f6700aac42d420d1e99b75cf5050ed120a7716
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-04-17 20:36:36 +0530

    widgets: add BoxDecoration with borderRadius and shadow to IconBox

--- lib/widgets/icon_box.dart
+++ lib/widgets/icon_box.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/theme/color.dart';
 
 class IconBox extends StatelessWidget {
   const IconBox({
     Key? key,
     required this.child,
     this.bgColor,
+    this.onTap,
+    this.borderColor = Colors.transparent,
+    this.radius = 50,
     this.padding = 5,
   }) : super(key: key);
 
   final Widget child;
   final Color? bgColor;
+  final Color borderColor;
+  final GestureTapCallback? onTap;
+  final double radius;
   final double padding;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(padding),
-      color: bgColor,
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(radius),
+        border: Border.all(color: borderColor),
+      ),
       child: child,
     );
   }
