ee42bcc147421c524090e65f5b22c35026737a81
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-04-20 21:42:14 +0530

    widgets: add prefixIcon and hintText properties to RoundTextBox

--- lib/widgets/round_textbox.dart
+++ lib/widgets/round_textbox.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class RoundTextBox extends StatelessWidget {
-  const RoundTextBox({Key? key}) : super(key: key);
+  const RoundTextBox({
+    Key? key,
+    this.hintText = "",
+    this.prefixIcon,
+  }) : super(key: key);
+
+  final String hintText;
+  final Widget? prefixIcon;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextField(),
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: prefixIcon,
+          hintText: hintText,
+        ),
+      ),
     );
   }
 }
