27349ca24c25ec46e214cdbf5cda0174f488424b
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-04-21 10:09:51 +0530

    widgets: add controller and borderless input decoration to RoundTextBox

--- lib/widgets/round_textbox.dart
+++ lib/widgets/round_textbox.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/theme/color.dart';
 
 class RoundTextBox extends StatelessWidget {
   const RoundTextBox({
     Key? key,
     this.hintText = "",
     this.prefixIcon,
+    this.controller,
   }) : super(key: key);
 
   final String hintText;
   final Widget? prefixIcon;
+  final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: TextField(
+        controller: controller,
         decoration: InputDecoration(
           prefixIcon: prefixIcon,
+          border: InputBorder.none,
           hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
         ),
       ),
     );
