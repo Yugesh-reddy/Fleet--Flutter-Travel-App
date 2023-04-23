97809ccf82c3c2c6bd526467a75056aadbd22393
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-04-23 15:09:47 +0530

    widgets: finalize lib/widgets/round_textbox.dart with full styling

--- lib/widgets/round_textbox.dart
+++ lib/widgets/round_textbox.dart
@@ -2,34 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:travel_app/theme/color.dart';
 
 class RoundTextBox extends StatelessWidget {
-  const RoundTextBox({
-    Key? key,
-    this.hintText = "",
-    this.prefixIcon,
-    this.controller,
-  }) : super(key: key);
-
-  final String hintText;
+  RoundTextBox({ Key? key, this.prefixIcon, this.hintText, this.controller}) : super(key: key);
   final Widget? prefixIcon;
+  final String? hintText;
   final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 40,
-      decoration: BoxDecoration(
-        color: textBoxColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          prefixIcon: prefixIcon,
-          border: InputBorder.none,
-          hintText: hintText,
-          hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
+    return 
+      Container(
+        height: 40,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade200,
+          borderRadius: BorderRadius.circular(50)
         ),
-      ),
-    );
+        child: TextField(
+          controller: controller,
+          style: TextStyle(color: textColor),
+          decoration: InputDecoration(
+            prefixIcon: prefixIcon,
+            border: InputBorder.none,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey, fontSize: 17)),
+        ),
+      );
   }
 }
