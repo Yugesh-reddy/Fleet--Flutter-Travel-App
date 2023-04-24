52c6d4ac734d145bc8791d805cd4d8695b5342a9
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-04-24 09:28:25 +0530

    widgets: implement ClipRRect and radius parameter in CustomImage

--- lib/widgets/custom_image.dart
+++ lib/widgets/custom_image.dart
@@ -6,21 +6,40 @@ class CustomImage extends StatelessWidget {
     Key? key,
     this.width = 100,
     this.height = 100,
+    this.bgColor,
+    this.borderWidth = 0,
+    this.borderColor,
+    this.trRadius = 0,
+    this.tlRadius = 0,
+    this.brRadius = 0,
+    this.blRadius = 0,
+    this.radius = 0,
     this.fit = BoxFit.cover,
   }) : super(key: key);
 
   final String image;
   final double width;
   final double height;
+  final double borderWidth;
+  final Color? borderColor;
+  final Color? bgColor;
+  final double trRadius;
+  final double tlRadius;
+  final double brRadius;
+  final double blRadius;
+  final double radius;
   final BoxFit fit;
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      image,
-      width: width,
-      height: height,
-      fit: fit,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(radius),
+      child: Image.network(
+        image,
+        width: width,
+        height: height,
+        fit: fit,
+      ),
     );
   }
 }
