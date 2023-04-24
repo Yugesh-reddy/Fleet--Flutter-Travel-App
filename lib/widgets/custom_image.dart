05670b7d542193e7105fef93f213cdeacb5ac4a8
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-04-24 09:00:53 +0530

    widgets: add width, height, and fit properties to CustomImage

--- lib/widgets/custom_image.dart
+++ lib/widgets/custom_image.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 
 class CustomImage extends StatelessWidget {
-  const CustomImage(this.image, {Key? key}) : super(key: key);
+  const CustomImage(
+    this.image, {
+    Key? key,
+    this.width = 100,
+    this.height = 100,
+    this.fit = BoxFit.cover,
+  }) : super(key: key);
+
   final String image;
+  final double width;
+  final double height;
+  final BoxFit fit;
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(image);
+    return Image.network(
+      image,
+      width: width,
+      height: height,
+      fit: fit,
+    );
   }
 }
