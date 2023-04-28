91038262dea7bf902ab1236a208e468443a178bd
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-04-28 23:25:27 +0530

    widgets: finalize lib/widgets/custom_image.dart implementation

--- lib/widgets/custom_image.dart
+++ lib/widgets/custom_image.dart
@@ -1,67 +1,81 @@
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:travel_app/theme/color.dart';
 
 class CustomImage extends StatelessWidget {
-  const CustomImage(
-    this.image, {
-    Key? key,
-    this.width = 100,
-    this.height = 100,
-    this.bgColor,
-    this.borderWidth = 0,
-    this.borderColor,
-    this.trRadius = 0,
-    this.tlRadius = 0,
-    this.brRadius = 0,
-    this.blRadius = 0,
-    this.radius = 0,
-    this.fit = BoxFit.cover,
-    this.isNetwork = true,
-  }) : super(key: key);
-
+  CustomImage(this.image, {this.width = 100, this.height = 100, this.bgColor, 
+    this.borderWidth = 0, this.borderColor, this.trBackground = false, this.fit = BoxFit.cover,
+    this.isNetwork = true, this.radius = 50, this.isShadow = true});
   final String image;
   final double width;
   final double height;
   final double borderWidth;
+  final bool isShadow;
   final Color? borderColor;
   final Color? bgColor;
-  final double trRadius;
-  final double tlRadius;
-  final double brRadius;
-  final double blRadius;
+  final bool trBackground;
+  final bool isNetwork;
   final double radius;
   final BoxFit fit;
-  final bool isNetwork;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        color: bgColor,
-        borderRadius: radius > 0
-            ? BorderRadius.circular(radius)
-            : BorderRadius.only(
-                topLeft: Radius.circular(tlRadius),
-                topRight: Radius.circular(trRadius),
-                bottomLeft: Radius.circular(blRadius),
-                bottomRight: Radius.circular(brRadius),
+    return 
+      Container(width: width, height: height,
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(radius),
+          boxShadow: [
+            if(isShadow) BoxShadow(
+              color: shadowColor.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 1,
+              offset: Offset(0, 1), // changes position of shadow
+            ),
+          ],
+        ),
+        child: isNetwork ?
+          CachedNetworkImage(
+            imageUrl: image,
+            placeholder: (context, url) => BlankImageWidget(),
+            errorWidget: (context, url, error) => BlankImageWidget(),
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(radius),
+                image: DecorationImage(
+                  image: imageProvider, fit: fit),
               ),
-        border: Border.all(color: borderColor ?? Colors.transparent, width: borderWidth),
-      ),
-      child: ClipRRect(
-        borderRadius: radius > 0
-            ? BorderRadius.circular(radius)
-            : BorderRadius.only(
-                topLeft: Radius.circular(tlRadius),
-                topRight: Radius.circular(trRadius),
-                bottomLeft: Radius.circular(blRadius),
-                bottomRight: Radius.circular(brRadius),
-              ),
-        child: isNetwork
-            ? Image.network(image, width: width, height: height, fit: fit)
-            : Image.asset(image, width: width, height: height, fit: fit),
-      ),
+            ),
+          )
+        : 
+        Image(
+          image: AssetImage(image), 
+          fit: fit,
+        )
+      );
+  }
+}
+
+class BlankImageWidget extends StatefulWidget {
+  const BlankImageWidget({ Key? key }) : super(key: key);
+
+  @override
+  _BlankImageWidgetState createState() => _BlankImageWidgetState();
+}
+
+class _BlankImageWidgetState extends State<BlankImageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: Center(child: SizedBox(
+        child: Card(
+          margin: EdgeInsets.zero,
+          clipBehavior: Clip.antiAlias,
+          elevation: 0.0,
+        ),
+      )),
     );
   }
 }
