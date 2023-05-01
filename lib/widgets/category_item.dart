3a1d59c6b711402e951943e80ed7671c098ca8aa
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-01 13:53:36 +0530

    widgets: implement isSelected styling and active indicator on CategoryItem

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:travel_app/theme/color.dart';
 
 class CategoryItem extends StatelessWidget {
-  const CategoryItem({Key? key, required this.data, this.onTap}) : super(key: key);
+  const CategoryItem({
+    Key? key,
+    required this.data,
+    this.isSelected = false,
+    this.onTap,
+  }) : super(key: key);
+
   final data;
+  final bool isSelected;
   final GestureTapCallback? onTap;
 
   @override
@@ -12,7 +20,19 @@ class CategoryItem extends StatelessWidget {
       onTap: onTap,
       child: Column(
         children: [
-          SvgPicture.asset(data["icon"]),
+          Container(
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: isSelected ? primary : cardColor,
+              shape: BoxShape.circle,
+            ),
+            child: SvgPicture.asset(
+              data["icon"],
+              color: isSelected ? Colors.white : darker,
+              width: 25,
+              height: 25,
+            ),
+          ),
           SizedBox(height: 5),
           Text(data["name"]),
         ],
