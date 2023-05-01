d55b25d076af225abdce337d79877f6aec95c2c8
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-01 11:39:10 +0530

    widgets: add SVG category icon and label to CategoryItem

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CategoryItem extends StatelessWidget {
-  const CategoryItem({Key? key, required this.data}) : super(key: key);
+  const CategoryItem({Key? key, required this.data, this.onTap}) : super(key: key);
   final data;
+  final GestureTapCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(data["name"]),
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          SvgPicture.asset(data["icon"]),
+          SizedBox(height: 5),
+          Text(data["name"]),
+        ],
+      ),
     );
   }
 }
