c844eb57e51c2e14820d284cf6934d824518cec3
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-02 17:17:33 +0530

    widgets: finalize lib/widgets/category_item.dart component

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,59 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:travel_app/theme/color.dart';
 
 class CategoryItem extends StatelessWidget {
-  const CategoryItem({
-    Key? key,
-    required this.data,
-    this.isSelected = false,
-    this.onTap,
-  }) : super(key: key);
-
+  CategoryItem({ Key? key, required this.data, this.onTap, this.color = primary }) : super(key: key);
   final data;
-  final bool isSelected;
+  final Color color;
   final GestureTapCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Column(
-        children: [
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 500),
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: isSelected ? primary : cardColor,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: shadowColor.withOpacity(0.05),
-                  spreadRadius: .5,
-                  blurRadius: .5,
-                  offset: Offset(0, 1),
-                ),
-              ],
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.fastOutSlowIn,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(right: 10),
+        width: 90, height: 90,
+        decoration: BoxDecoration(
+          color: cardColor,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: shadowColor.withOpacity(0.1),
+              spreadRadius: .5,
+              blurRadius: .5,
+              offset: Offset(0, 1), // changes position of shadow
             ),
-            child: SvgPicture.asset(
-              data["icon"],
-              color: isSelected ? Colors.white : darker,
-              width: 20,
-              height: 20,
+          ],
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: color.withOpacity(.2)
+              ),
+              child: Icon(data["icon"], size: 25, color: color)
             ),
-          ),
-          SizedBox(height: 5),
-          Text(
-            data["name"],
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontSize: 13,
-              color: isSelected ? primary : darker,
-              fontWeight: isSelected ? FontWeight.w600 : FontWeight.w500,
+            SizedBox(height: 5,),
+            Expanded(
+              child: Text(data["name"], maxLines: 1, overflow: TextOverflow.ellipsis, 
+                style: TextStyle(fontSize: 13, color: textColor),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
