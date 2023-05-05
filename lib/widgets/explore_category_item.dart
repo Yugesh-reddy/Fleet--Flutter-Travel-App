6a922c77090b4c8fec7776fe6235e8d29ed4881d
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-05 09:13:48 +0530

    widgets: add icon and title layout in ExploreCategoryItem

--- lib/widgets/explore_category_item.dart
+++ lib/widgets/explore_category_item.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:travel_app/theme/color.dart';
 
 class ExploreCategoryItem extends StatelessWidget {
-  const ExploreCategoryItem({Key? key, required this.data}) : super(key: key);
+  const ExploreCategoryItem({
+    Key? key,
+    required this.data,
+    this.onTap,
+  }) : super(key: key);
+
   final data;
+  final GestureTapCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(data["name"]),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            SvgPicture.asset(data["icon"]),
+            SizedBox(width: 8),
+            Text(data["name"]),
+          ],
+        ),
+      ),
     );
   }
 }
