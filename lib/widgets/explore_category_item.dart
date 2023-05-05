a3ad99d662a9d6ca7eb107e6ce88ccf2b0b0bbb9
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-05 14:30:13 +0530

    widgets: style ExploreCategoryItem with card background and border

--- lib/widgets/explore_category_item.dart
+++ lib/widgets/explore_category_item.dart
@@ -6,10 +6,14 @@ class ExploreCategoryItem extends StatelessWidget {
   const ExploreCategoryItem({
     Key? key,
     required this.data,
+    this.bgColor = Colors.white,
+    this.color = primary,
     this.onTap,
   }) : super(key: key);
 
   final data;
+  final Color? bgColor;
+  final Color color;
   final GestureTapCallback? onTap;
 
   @override
@@ -18,9 +22,21 @@ class ExploreCategoryItem extends StatelessWidget {
       onTap: onTap,
       child: Container(
         padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: shadowColor.withOpacity(0.05),
+              spreadRadius: .5,
+              blurRadius: .5,
+              offset: Offset(0, 1),
+            ),
+          ],
+        ),
         child: Row(
           children: [
-            SvgPicture.asset(data["icon"]),
+            SvgPicture.asset(data["icon"], color: color),
             SizedBox(width: 8),
             Text(data["name"]),
           ],
