328343cf819357af3caece4010b0b853b6a3b26f
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-06 22:30:37 +0530

    widgets: add title, location, and info column to PopularItem

--- lib/widgets/popular_item.dart
+++ lib/widgets/popular_item.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'custom_image.dart';
 
 class PopularItem extends StatelessWidget {
-  const PopularItem({Key? key, required this.data}) : super(key: key);
+  const PopularItem({Key? key, required this.data, this.onTap}) : super(key: key);
   final data;
+  final GestureTapCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: CustomImage(data["image"]),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 200,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomImage(data["image"], radius: 15, height: 140, width: 200),
+            SizedBox(height: 10),
+            Text(data["name"]),
+            Text(data["location"]),
+          ],
+        ),
+      ),
     );
   }
 }
