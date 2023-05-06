f09e519aab53b1af5573a0a53d4d70d1db36471f
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-06 10:31:13 +0530

    widgets: finalize lib/widgets/explore_category_item.dart

--- lib/widgets/explore_category_item.dart
+++ lib/widgets/explore_category_item.dart
@@ -1,47 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:travel_app/theme/color.dart';
 
 class ExploreCategoryItem extends StatelessWidget {
-  const ExploreCategoryItem({
-    Key? key,
-    required this.data,
-    this.bgColor = Colors.white,
-    this.color = primary,
-    this.onTap,
-  }) : super(key: key);
-
+  ExploreCategoryItem({ Key? key, required this.data, this.bgColor = Colors.white, this.color = primary, this.selected = false, this.onTap}) : super(key: key);
   final data;
-  final Color? bgColor;
+  final Color bgColor;
   final Color color;
+  final bool selected;
   final GestureTapCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: bgColor,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: shadowColor.withOpacity(0.05),
-              spreadRadius: .5,
-              blurRadius: .5,
-              offset: Offset(0, 1),
-            ),
-          ],
+    return 
+      GestureDetector(
+        onTap: onTap,
+        child: 
+        Container(
+          height: 40, width: 70,
+          color: appBgColor,
+          margin: EdgeInsets.only(right: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children : [
+              Flexible(
+                child: Text(data["name"], maxLines: 1, overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: selected ? textColor : labelColor),
+                ),
+              ),
+              Visibility(
+                visible: selected,
+                child: Container(
+                  margin: EdgeInsets.only(top: 4),
+                  width: double.infinity, height: 2, 
+                  decoration: BoxDecoration(
+                    color: secondary
+                  ),
+                ),
+              ),
+            ]
+          ),
         ),
-        child: Row(
-          children: [
-            SvgPicture.asset(data["icon"], color: color),
-            SizedBox(width: 8),
-            Text(data["name"]),
-          ],
-        ),
-      ),
-    );
+      );
   }
 }
