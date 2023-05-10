a3b36c4e43e1eb042138aafc7f555d4c0cd0f01a
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-10 19:38:10 +0530

    widgets: add favorite heart button overlay in PopularItem

--- lib/widgets/popular_item.dart
+++ lib/widgets/popular_item.dart
@@ -1,46 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'custom_image.dart';
+import 'icon_box.dart';
 import 'package:travel_app/theme/color.dart';
 
 class PopularItem extends StatelessWidget {
-  const PopularItem({Key? key, required this.data, this.onTap}) : super(key: key);
+  const PopularItem({
+    Key? key,
+    required this.data,
+    this.width = 200,
+    this.height = 300,
+    this.radius = 15,
+    this.onTap,
+    this.onFavoriteTap,
+  }) : super(key: key);
+
   final data;
+  final double width;
+  final double height;
+  final double radius;
   final GestureTapCallback? onTap;
+  final GestureTapCallback? onFavoriteTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: 200,
+        width: width,
+        height: height,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Stack(
               children: [
-                CustomImage(data["image"], radius: 15, height: 140, width: 200),
+                CustomImage(data["image"], radius: radius, height: 140, width: width),
                 Positioned(
-                  top: 10,
+                  bottom: 10,
                   right: 10,
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: primary,
-                      borderRadius: BorderRadius.circular(5),
+                  child: IconBox(
+                    bgColor: Colors.white,
+                    onTap: onFavoriteTap,
+                    child: SvgPicture.asset(
+                      "assets/icons/heart.svg",
+                      color: data["is_favorited"] ? Colors.red : primary,
                     ),
-                    child: Text(data["price"], style: TextStyle(color: Colors.white)),
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Text(data["name"]),
-            Row(
-              children: [
-                SvgPicture.asset("assets/icons/star.svg", width: 14, height: 14),
-                SizedBox(width: 4),
-                Text(data["rate"]),
               ],
             ),
           ],
