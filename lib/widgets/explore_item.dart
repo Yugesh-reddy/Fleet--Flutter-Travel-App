21b01f979eeae76579d6654d709e14fa1538e066
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-13 12:13:15 +0530

    widgets: add horizontal thumbnail layout to ExploreItem

--- lib/widgets/explore_item.dart
+++ lib/widgets/explore_item.dart
@@ -2,13 +2,30 @@ import 'package:flutter/material.dart';
 import 'custom_image.dart';
 
 class ExploreItem extends StatelessWidget {
-  const ExploreItem({Key? key, required this.data}) : super(key: key);
+  const ExploreItem({Key? key, required this.data, this.onTap}) : super(key: key);
   final data;
+  final GestureTapCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: CustomImage(data["image"]),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Row(
+          children: [
+            CustomImage(data["image"], radius: 10, width: 80, height: 80),
+            SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(data["name"]),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
