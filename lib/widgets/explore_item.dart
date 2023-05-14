dadd307e99451ea3885609b6b78abce36e204813
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-14 19:55:20 +0530

    widgets: add price badge and favorite heart to ExploreItem

--- lib/widgets/explore_item.dart
+++ lib/widgets/explore_item.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'custom_image.dart';
+import 'package:travel_app/theme/color.dart';
 
 class ExploreItem extends StatelessWidget {
-  const ExploreItem({Key? key, required this.data, this.onTap}) : super(key: key);
+  const ExploreItem({
+    Key? key,
+    required this.data,
+    this.onTap,
+    this.onFavoriteTap,
+  }) : super(key: key);
+
   final data;
   final GestureTapCallback? onTap;
+  final GestureTapCallback? onFavoriteTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
+        padding: EdgeInsets.all(10),
         child: Row(
           children: [
             CustomImage(data["image"], radius: 10, width: 80, height: 80),
@@ -21,8 +30,7 @@ class ExploreItem extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(data["name"]),
-                  Text(data["location"]),
-                  Text(data["rate"]),
+                  Text(data["price"]),
                 ],
               ),
             ),
