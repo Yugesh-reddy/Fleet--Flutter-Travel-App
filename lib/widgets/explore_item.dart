31f00345f006e0eed16b1c3b3cc2b302d7ae658c
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-15 16:07:15 +0530

    widgets: finalize lib/widgets/explore_item.dart component

--- lib/widgets/explore_item.dart
+++ lib/widgets/explore_item.dart
@@ -1,44 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'custom_image.dart';
-import 'package:travel_app/theme/color.dart';
 
 class ExploreItem extends StatelessWidget {
-  const ExploreItem({
-    Key? key,
-    required this.data,
-    this.onTap,
-    this.onFavoriteTap,
-  }) : super(key: key);
-
+  ExploreItem({ Key? key, required this.data, this.onTap, this.raduis = 10 }) : super(key: key);
   final data;
+  final double raduis;
   final GestureTapCallback? onTap;
-  final GestureTapCallback? onFavoriteTap;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        margin: EdgeInsets.only(bottom: 10),
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: shadowColor.withOpacity(0.1),
-              spreadRadius: 1,
-              blurRadius: 1,
-              offset: Offset(0, 1),
+    return Container(
+      width: 100,
+      height: 150,
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(raduis)
+      ),
+      child: Stack(
+        children: [
+          Container(
+            child: CustomImage(data["image"],
+              radius: raduis, width: double.infinity, height: double.infinity,
             ),
-          ],
-        ),
-        child: Row(
-          children: [
-            CustomImage(data["image"], radius: 10, width: 80, height: 80),
-          ],
-        ),
+          ),
+          Container(
+            width: double.infinity, height: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(raduis),
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.black.withOpacity(.5),
+                  Colors.white.withOpacity(.01),
+                ]
+              )
+            ),
+          ),
+          Positioned(
+            bottom: 12, left: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(data["name"], style: TextStyle(fontSize: 14, color: Colors.white, fontWeight: FontWeight.w500,),),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
