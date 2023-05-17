9aa656611d28678de47e09b273a1fa939f5b94b2
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-17 17:33:37 +0530

    widgets: add badge count and container padding to NotificationBox

--- lib/widgets/notification_box.dart
+++ lib/widgets/notification_box.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:badges/badges.dart' as badges;
 
 class NotificationBox extends StatelessWidget {
-  const NotificationBox({Key? key}) : super(key: key);
+  const NotificationBox({
+    Key? key,
+    this.onTap,
+    this.notifiedNumber = 0,
+  }) : super(key: key);
+
+  final GestureTapCallback? onTap;
+  final int notifiedNumber;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SvgPicture.asset("assets/icons/bell.svg"),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: SvgPicture.asset("assets/icons/bell.svg"),
+      ),
     );
   }
 }
