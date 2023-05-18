67803316af3445855c5086dc183fc5886fecb592
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-18 09:20:37 +0530

    widgets: add rounded container border and badge positioning to NotificationBox

--- lib/widgets/notification_box.dart
+++ lib/widgets/notification_box.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:badges/badges.dart' as badges;
+import 'package:travel_app/theme/color.dart';
 
 class NotificationBox extends StatelessWidget {
   const NotificationBox({
     Key? key,
     this.onTap,
+    this.size = 5,
     this.notifiedNumber = 0,
   }) : super(key: key);
 
   final GestureTapCallback? onTap;
   final int notifiedNumber;
+  final double size;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        child: SvgPicture.asset("assets/icons/bell.svg"),
+        padding: EdgeInsets.all(size),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.grey.withOpacity(.3)),
+        ),
+        child: SvgPicture.asset("assets/icons/bell.svg", width: 20, height: 20),
       ),
     );
   }
