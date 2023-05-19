a38a34dd6d820e350f7f02490e66e4ae79694c47
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-19 17:55:44 +0530

    widgets: finalize lib/widgets/notification_box.dart implementation

--- lib/widgets/notification_box.dart
+++ lib/widgets/notification_box.dart
@@ -1,31 +1,35 @@
+import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:badges/badges.dart' as badges;
 import 'package:travel_app/theme/color.dart';
 
 class NotificationBox extends StatelessWidget {
-  const NotificationBox({
-    Key? key,
-    this.onTap,
-    this.size = 5,
-    this.notifiedNumber = 0,
-  }) : super(key: key);
-
+  NotificationBox({ Key? key, this.onTap, this.notifiedNumber = 0}) : super(key: key);
   final GestureTapCallback? onTap;
   final int notifiedNumber;
-  final double size;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: EdgeInsets.all(size),
+        padding: EdgeInsets.all(5),
         decoration: BoxDecoration(
           shape: BoxShape.circle,
+          color: appBarColor,
           border: Border.all(color: Colors.grey.withOpacity(.3)),
         ),
-        child: SvgPicture.asset("assets/icons/bell.svg", width: 20, height: 20),
+        child: notifiedNumber > 0 ? badges.Badge(
+          position: badges.BadgePosition.topEnd(top: -7, end: 2),
+          badgeStyle: badges.BadgeStyle(
+            badgeColor: actionColor,
+            padding: EdgeInsets.all(3),
+          ),
+          badgeContent: Text('', style: TextStyle(color: Colors.white),),
+          child: SvgPicture.asset("assets/icons/bell.svg", width: 25, height: 25,)
+        )
+        :
+        SvgPicture.asset("assets/icons/bell.svg")
       ),
     );
   }
