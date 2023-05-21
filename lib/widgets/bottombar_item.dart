1504ed44004a17d59d526590db8bd4160c37db3f
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-21 10:46:55 +0530

    widgets: implement isActive color tinting in BottomBarItem

--- lib/widgets/bottombar_item.dart
+++ lib/widgets/bottombar_item.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:travel_app/theme/color.dart';
 
 class BottomBarItem extends StatelessWidget {
-  const BottomBarItem(this.icon, {Key? key}) : super(key: key);
+  const BottomBarItem(
+    this.icon, {
+    Key? key,
+    this.isActive = false,
+    this.onTap,
+  }) : super(key: key);
+
   final String icon;
+  final bool isActive;
+  final GestureTapCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SvgPicture.asset(icon),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: SvgPicture.asset(
+          icon,
+          color: isActive ? primary : inActiveColor,
+        ),
+      ),
     );
   }
 }
