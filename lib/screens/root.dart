b1cf2e19b5805366f4c0166f9ccce423a132846f
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-28 14:33:11 +0530

    screens: create lib/screens/root.dart scaffold skeleton

---
+++ lib/screens/root.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({Key? key}) : super(key: key);
+
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Root Screen")),
+    );
+  }
+}
