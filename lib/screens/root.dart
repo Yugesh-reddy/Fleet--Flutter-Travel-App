25814bf2a099da1029100259a7c1183ea231c043
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-29 19:05:16 +0530

    screens: add elevation shadows and bar styling in RootApp

--- lib/screens/root.dart
+++ lib/screens/root.dart
@@ -21,9 +21,22 @@ class _RootAppState extends State<RootApp> {
         width: double.infinity,
         decoration: BoxDecoration(
           color: bottomBarColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: shadowColor.withOpacity(0.1),
+              blurRadius: 1,
+              spreadRadius: 1,
+              offset: Offset(0, 1),
+            ),
+          ],
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             BottomBarItem("assets/icons/home.svg", isActive: activeTab == 0, onTap: () => setState(() => activeTab = 0)),
             BottomBarItem("assets/icons/search.svg", isActive: activeTab == 1, onTap: () => setState(() => activeTab = 1)),
