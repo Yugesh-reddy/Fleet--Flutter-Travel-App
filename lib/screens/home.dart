14c465f480f8aaef7274f421253b5cb45a006741
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-05-30 09:27:54 +0530

    screens: implement user greeting and avatar in HomePage header

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/theme/color.dart';
+import 'package:travel_app/utils/data.dart';
+import 'package:travel_app/widgets/custom_image.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,9 +16,24 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: appBgColor,
       body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 15),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Home Page"),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Hello,", style: TextStyle(color: darker, fontSize: 14)),
+                    Text(profile["name"] as String, style: TextStyle(color: primary, fontSize: 20, fontWeight: FontWeight.bold)),
+                  ],
+                ),
+                CustomImage(profile["image"] as String, width: 45, height: 45, radius: 50),
+              ],
+            ),
           ],
         ),
       ),
