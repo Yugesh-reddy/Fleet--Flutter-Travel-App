3825c944813fdf9424b0c884d1dd232d410822d6
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-06-10 17:56:49 +0530

    screens: add explore header with title and search bar in ExplorePage

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:travel_app/theme/color.dart';
+import 'package:travel_app/widgets/round_textbox.dart';
 
 class ExplorePage extends StatefulWidget {
   const ExplorePage({Key? key}) : super(key: key);
@@ -13,7 +15,25 @@ class _ExplorePageState extends State<ExplorePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: appBgColor,
-      body: Center(child: Text("Explore Page")),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 50),
+            Text("Find your best
+destination", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: primary)),
+            SizedBox(height: 20),
+            RoundTextBox(
+              hintText: "Search destinations...",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset("assets/icons/search.svg", color: darker),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
