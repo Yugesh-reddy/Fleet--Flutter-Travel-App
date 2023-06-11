7e6ebba88ad33b0604383cc67921035cd5b1628a
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-06-11 10:31:39 +0530

    screens: add destination cards list section in ExplorePage

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:travel_app/theme/color.dart';
 import 'package:travel_app/utils/data.dart';
-import 'package:travel_app/widgets/explore_category_item.dart';
-import 'package:travel_app/widgets/round_textbox.dart';
+import 'package:travel_app/widgets/explore_item.dart';
 
 class ExplorePage extends StatefulWidget {
   const ExplorePage({Key? key}) : super(key: key);
@@ -13,8 +11,6 @@ class ExplorePage extends StatefulWidget {
 }
 
 class _ExplorePageState extends State<ExplorePage> {
-  int selectedCategory = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,24 +21,11 @@ class _ExplorePageState extends State<ExplorePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 50),
-            Text("Find your best
-destination", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: primary)),
-            SizedBox(height: 20),
-            Container(
-              height: 45,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: exploreCategories.length,
-                itemBuilder: (context, index) => Container(
-                  margin: EdgeInsets.only(right: 10),
-                  child: ExploreCategoryItem(
-                    data: exploreCategories[index],
-                    bgColor: selectedCategory == index ? primary : Colors.white,
-                    color: selectedCategory == index ? Colors.white : primary,
-                    onTap: () => setState(() => selectedCategory = index),
-                  ),
-                ),
-              ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: explores.length,
+              itemBuilder: (context, index) => ExploreItem(data: explores[index]),
             ),
           ],
         ),
