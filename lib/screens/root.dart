8ecff3213d1e63a140caddc24c1f0b54b716893e
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-06-11 18:34:33 +0530

    screens: add animated page body switching and tab transitions in RootApp

--- lib/screens/root.dart
+++ lib/screens/root.dart
@@ -1,48 +1,133 @@
+
 import 'package:flutter/material.dart';
 import 'package:travel_app/theme/color.dart';
+import 'package:travel_app/utils/constant.dart';
 import 'package:travel_app/widgets/bottombar_item.dart';
-import 'home.dart';
+
 import 'explore.dart';
+import 'home.dart';
+
 
 class RootApp extends StatefulWidget {
-  const RootApp({Key? key}) : super(key: key);
+  const RootApp({ Key? key }) : super(key: key);
 
   @override
   _RootAppState createState() => _RootAppState();
 }
 
-class _RootAppState extends State<RootApp> {
-  int activeTab = 0;
+class _RootAppState extends State<RootApp>  with TickerProviderStateMixin {
+  int activeTab = 1;
+  List barItems = [
+
+    {
+      "icon" : "assets/icons/navigation.svg",
+      "active_icon" : "assets/icons/navigation.svg",
+      "page" : ExplorePage(),
+      "title" : ""
+    },
+    {
+      "icon" : "assets/icons/home.svg",
+      "active_icon" : "assets/icons/home.svg",
+      "page" : HomePage(),
+      "title" : ""
+    },
+    {
+      "icon" : "assets/icons/settings.svg",
+      "active_icon" : "assets/icons/settings.svg",
+      "page" : Container(),
+      "title" : ""
+    },
+  ];
+//====== set animation=====
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: ANIMATED_BODY_MS),
+    vsync: this,
+  );
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+     _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.stop();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  animatedPage(page){
+    return FadeTransition(
+      child: page,
+      opacity: _animation
+    );
+  }
+
+  void onPageChanged(int index) {
+    _controller.reset();
+    setState(() {
+      activeTab = index;
+    });
+    _controller.forward();
+  }
+
+//====== end set animation=====
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: appBgColor,
-      bottomNavigationBar: Container(
-        height: 75,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: bottomBarColor,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            BottomBarItem("assets/icons/home.svg", isActive: activeTab == 0, onTap: () => setState(() => activeTab = 0)),
-            BottomBarItem("assets/icons/search.svg", isActive: activeTab == 1, onTap: () => setState(() => activeTab = 1)),
-            BottomBarItem("assets/icons/heart.svg", isActive: activeTab == 2, onTap: () => setState(() => activeTab = 2)),
-            BottomBarItem("assets/icons/setting.svg", isActive: activeTab == 3, onTap: () => setState(() => activeTab = 3)),
-          ],
-        ),
-      ),
-      body: IndexedStack(
+      body: getBarPage(),
+      // bottomNavigationBar: getBottomBar1()
+      floatingActionButton: getBottomBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterDocked,
+    );
+  }
+
+  Widget getBarPage(){
+    return 
+      IndexedStack(
         index: activeTab,
-        children: [
-          HomePage(),
-          ExplorePage(),
-          Center(child: Text("Favorites")),
-          Center(child: Text("Settings")),
-        ],
+        children: 
+          List.generate(barItems.length, 
+            (index) => animatedPage(barItems[index]["page"])
+          )
+      );
+  }
+
+  Widget getBottomBar() {
+    return Container(
+      height: 55, width: double.infinity,
+      margin: EdgeInsets.fromLTRB(25, 0, 25, 0),
+      decoration: BoxDecoration(
+        color: bottomBarColor,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: shadowColor.withOpacity(0.1),
+            blurRadius: 1,
+            spreadRadius: 1,
+            offset: Offset(0, 1)
+          )
+        ]
       ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: 
+          List.generate(barItems.length, 
+            (index) => BottomBarItem( activeTab == index ? barItems[index]["active_icon"] : barItems[index]["icon"], "", isActive: activeTab == index, activeColor: primary,
+              onTap: (){
+                onPageChanged(index);
+              },
+            )
+          )
+        ),
     );
   }
 }
