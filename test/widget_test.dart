dc9c90adc0679450718e99f0ecb808ebed521870
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2023-06-12 21:43:19 +0530

    test: create base widget test in test/widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:travel_app/main.dart';
+
+void main() {
+  testWidgets('App smoke test', (WidgetTester tester) async {
+    // Basic test setup
+    expect(1 + 1, equals(2));
+  });
+}
