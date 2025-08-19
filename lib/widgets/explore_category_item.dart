11612c2ac2fb6c02748a81738d7c407e9b94546e
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2025-08-19 15:51:46 +0530

    Flutter travel app

---
+++ lib/widgets/explore_category_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/theme/color.dart';
+
+class ExploreCategoryItem extends StatelessWidget {
+  ExploreCategoryItem({ Key? key, required this.data, this.bgColor = Colors.white, this.color = primary, this.selected = false, this.onTap}) : super(key: key);
+  final data;
+  final Color bgColor;
+  final Color color;
+  final bool selected;
+  final GestureTapCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+      GestureDetector(
+        onTap: onTap,
+        child: 
+        Container(
+          height: 40, width: 70,
+          color: appBgColor,
+          margin: EdgeInsets.only(right: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children : [
+              Flexible(
+                child: Text(data["name"], maxLines: 1, overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: selected ? textColor : labelColor),
+                ),
+              ),
+              Visibility(
+                visible: selected,
+                child: Container(
+                  margin: EdgeInsets.only(top: 4),
+                  width: double.infinity, height: 2, 
+                  decoration: BoxDecoration(
+                    color: secondary
+                  ),
+                ),
+              ),
+            ]
+          ),
+        ),
+      );
+  }
+}
